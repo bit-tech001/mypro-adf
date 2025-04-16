019df4ef5f92eac38495c2af0987cd5545c306f1
bit-tech001 <tasus0833@gmail.com>
2025-04-16 12:57:43 +0530

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -183,7 +183,7 @@ export default function Page() {
                     location={project.location}
                     dates={project.dates}
                     image={project.image}
-                    links={project.links}
+                  
                   />
                 </BlurFade>
               ))}
